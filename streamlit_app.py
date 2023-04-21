f2e77a3979befebe4a9caa64af0d995f43e7f55b
Audi Rivai <53809969+ariv0004@users.noreply.github.com>
2023-04-21 20:08:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,11 @@ streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
+
+streamlit.header("Fruityvice Fruit to add")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('add new fruiet ', fruit_choice)
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
